8c7285dd4cabed025b3137845b93d3aed6f9e3fd
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-02 13:37:54 +0330

    codes belongs to connecting to database is moved to another module.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,20 @@
 const express = require("express");
-const { sequelize } = require("../configs/db.js");
 const { User } = require("./models/User");
 
 const app = express();
 
-sequelize
-  .authenticate()
-  .then(() => console.log("Connected to the database ..."))
-  .catch((err) => console.log("Can't connect to the database. Error: " + err));
+require('./startup/db')();
 
 app.get("/", (req, res) => {
   res.send("Hello express !!");
 });
-
-const PORT = process.env.PORT || 3000;
-
 app.get("/sync", (req, res) => {
   User.sync({ force: true })
     .then(() => res.send("Table created!"))
     .catch((err) => res.status(500).send("Error: " + err));
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}.`);
