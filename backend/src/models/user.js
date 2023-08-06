05fa76363760d5e63ab804ce4d7d6c3f2d4dd1d0
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-07 01:11:25 +0330

    validating added for users.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,49 +1,93 @@
-const { DataTypes, Model } = require("sequelize");
-const Joi = require('joi');
+const { DataTypes } = require("sequelize");
 const { sequelize } = require("../../configs/db.js");
+const Image = require('./image');
+const Joi = require('joi');
 
-class User extends Model {}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+const User = sequelize.define('User', {
+    userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 2,
+            max: 50
+        }
+    },
+    lastName:{
+        type: DataTypes.STRING,
+        validate: {
+            min: 2,
+            max: 50
+        }
+    },
+    PhoneNumber: {
+        type: DataTypes.STRING,
+        validate: {
+            isPhoneNumberFormat(value) {
+                const phoneNumberRegex = /^\d{11}$/;
+
+                if (!phoneNumberRegex.test(value)) {
+                    throw new Error('Invalid phone number format. Please use XXXX-XXX-XXXX.');
+                }
+            }
+        }
     },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isStrongPassword(value){
+                const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/;
+
+                if(!strongPasswordRegex.test(value))
+                    throw new Error('Its not strong password.');
+            }
+        }
+    },
+    emailIsVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    phoneNumberIsVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
     },
     createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
     },
     updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    sequelize,
-    modelName: "User",
-    tableName: "users",
-  }
-);
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    }
+});
+
+User.hasMany(Image, {
+    foreignKey: 'ImageId',
+});
 
 function userValidate(user) {
-    const schema = Joi.object({})
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(50).required(),
+        lastName: Joi.string().min(2).max(50).required(),
+        phoneNumber: Joi.string().pattern(/^\d{11}$/).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/).required()
+    })
     return schema.validate(user);
 }
 module.exports = { User, userValidate };
