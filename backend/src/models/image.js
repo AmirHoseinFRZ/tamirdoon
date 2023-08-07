9508812c0f37b5e9042ca70555e20eb9fdfa3591
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-07 17:17:42 +0330

    image model changed.

--- backend/src/models/image.js
+++ backend/src/models/image.js
@@ -2,11 +2,6 @@ const {sequelize} = require("../../configs/db");
 const {DataTypes} = require("sequelize");
 
 module.exports = sequelize.define('Image', {
-    imageId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
     filename: {
         type: DataTypes.STRING,
         allowNull: false,
