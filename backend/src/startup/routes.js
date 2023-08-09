ad62e2dc31bb7d887ffff7816310741d1101f2d0
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-08-09 12:48:24 +0330

    users router and cookie parser used.

--- backend/src/startup/routes.js
+++ backend/src/startup/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const test = require('../routes/test');
+const users = require('../routes/users');
 const error = require('../middlewares/error');
 
 module.exports = function (app){
     app.use(express.json());
+    app.use(cookieParser());
     app.use('/api/test', test);
+    app.use('/api/users', users);
     // another routes
     app.use(error);
 }
